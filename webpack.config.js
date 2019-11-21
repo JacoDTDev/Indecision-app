82f15b551f5c4791f6b472b14f879d8fff0d0cec
JacoDTDev <jacodtdev@gmail.com>
2019-11-21 13:58:22 +0200

    50. Installing & Configuring Webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+console.log(__dirname);
+
+
+module.exports = {
+  entry:'./src/app.js',
+    output: {
+        path: "C:\\Users\\datatim\\Desktop\\meteor-projects\\Indecision-app\\public",
+        filename: "bundle.js"
+    }
+};
