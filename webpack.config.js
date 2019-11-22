6c0c4280e14e09f643ba0caa10e3589041391e31
JacoDTDev <jacodtdev@gmail.com>
2019-11-22 10:39:54 +0200

    54. Setting up Babel with Webpack - to use jsx in webpack we must install babel - using babel loader we can now use jsx

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,12 @@ module.exports = {
     output: {
         path: "C:\\Users\\datatim\\Desktop\\meteor-projects\\Indecision-app\\public",
         filename: "bundle.js"
+    },
+    module:{
+      rules: [{
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude:/node_modules/
+      }]
     }
 };
